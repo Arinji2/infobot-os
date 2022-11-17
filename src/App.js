5f6aec8626d2094514b5f3cf7960ed26e7800e93
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-11-17 23:41:49 +0530

    Os Information

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import HeroPic from "./assets/pictures/hero.svg";
-
+import NavBar from "./components/navbar";
+import Pc from "./assets/pictures/Pc.jpg";
 import "./App.css";
 import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-br from-slate-400 to-black ">
+      <NavBar />
       <Hero />
+      <Os />
     </div>
   );
 }
 
 function Hero() {
   return (
-    <div className="relative flex flex-col md:flex-row items-center justify-center md:justify-between h-screen w-screen bg-gradient-to-br from-slate-400 to-black">
-      <div className=" h-screen w-screen  flex flex-col items-center justify-center gap-5 md:gap-20 relative text-left ">
-        <h1 className="m-3 text-5xl md:text-9xl text-yellow-400 mt-20 ">
+    <div className="relative flex flex-col md:flex-row items-center justify-center md:justify-between h-screen bg-transparent ">
+      <div className=" h-screen w-3/5 flex flex-col items-center justify-start gap-10 md:gap-20 relative ">
+        <h1 className="m-3 text-5xl md:text-8xl text-yellow-400 mt-20 ">
           InfoBot
         </h1>
         <h2 className="m-3 text-2xl md:text-4xl text-white text-center">
           Java Programming, done the Right Way
         </h2>
         <p
-          className="bg-transparent text-yellow-400 p-2 rounded-lg border-white border-2 scale-110 shadow-lg shadow-black hover:text-white hover:bg-yellow-400 hover:border-2 hover:border-yellow-400 transition-all ease-in-out duration-200 hover:cursor-pointer"
+          className="bg-yellow-400  text-[#3F434E] p-2 rounded-lg border-yellow-400 border-2 scale-110 shadow-lg shadow-black hover:text-yellow-400 hover:bg-transparent hover:border-2 hover:border-white transition-all ease-in-out duration-200 hover:cursor-pointer"
           onClick={() => {
             window.location.hostname = "os.infobot.tech";
           }}
@@ -30,9 +33,50 @@ function Hero() {
           Check Out the OS
         </p>
       </div>
-      <img src={HeroPic} alt={"Sync Pic"} className="w-2/3 h-2/3" />
-      <p className="pb-10"></p>
+      <div className="w-[60%] md:w-2/6 h-screen flex flex-col items-center justify-start mt-20">
+        <img src={HeroPic} alt={"Sync Pic"} />
+      </div>
     </div>
   );
 }
+
+function Os() {
+  return (
+    <div className="flex flex-col items-center text-white gap-3 justify-center text-center mt-20 md:mt-10">
+      <h3 className="text-5xl">
+        Meet the <span className="text-yellow-400">InfoBot OS</span>
+      </h3>
+      <p className="">An OS built to learn Java in Style and with Ease</p>
+      <img
+        src={Pc}
+        alt={Pc}
+        className="w-[80%] md:w-[55%] bg-transparent hover:md:w-[50%] transition-all ease-in-out duration-200"
+      />
+      <div className="mt-5 flex flex-row md:flex-nowrap flex-wrap justify-center items-center gap-4 m-4">
+        <div className="flex-col items-center justify-center sm:w-50 sm:h-50 md:w-2/5   shadow-md shadow-black rounded-lg p-2 hover:shadow-black hover:shadow-lg scale-90 hover:scale-100 transition-all ease-in-out duration-200">
+          <h4 className="text-yellow-400 text-xl">Pricing</h4>
+          <p>
+            InfoBot OS will always be free forever. Be assured that none of the
+            features will ever be locked behind a paywall.
+          </p>
+        </div>
+        <div className="flex-col items-center justify-center sm:w-50 sm:h-50 md:w-2/5   shadow-md shadow-black rounded-lg p-2 hover:shadow-black hover:shadow-lg scale-90 hover:scale-100 transition-all ease-in-out duration-200">
+          <h4 className="text-yellow-400 text-xl">Specifications</h4>
+          <p>
+            Nothing Required, other than a Web Browser. InfoBot OS is built for
+            everything, including mobile devices.
+          </p>
+        </div>
+        <div className="flex-col items-center justify-center sm:w-50 sm:h-50 md:w-2/5   shadow-md shadow-black rounded-lg p-2 hover:shadow-black hover:shadow-lg scale-90 hover:scale-100 transition-all ease-in-out duration-200">
+          <h4 className="text-yellow-400 text-xl">Security</h4>
+          <p>
+            Protected and Secured, InfoBot OS is protected from most attacks.
+            Use freely with no tension of popups or redirects.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,30 @@
 import React from "react";
 import Logo from "../assets/pictures/logo.png";
+import "../index.css";
 function NavBar() {
   return (
-    <div className="bg-transparent relative h-20 flex flex-row items-center justify-between">
-      <div className="flex flex-row items-center justify-evenly gap-3 border-r-2 border-white">
-        <img src={Logo} alt={"Logo"} className="h-16 w-16 ml-4" />
-        <p className="text-yellow-400 text-3xl mr-4 self-end mb-2">InfoBot</p>
+    <div
+      className="nav bg-gradient-to-r from-[#909EB2] to-[#49515B]  h-20  sticky top-0 z-10 border-b-2 border-white ml-5 mr-5 flex-row items-center justify-between"
+      id="nav"
+      onScroll={(el) => {
+        console.log(el.currentTarget.clientHeight);
+      }}
+    >
+      <div className="flex flex-col items-center justify-center w-20 h-20 ml-5">
+        <img src={Logo} alt={"Logo"} className="h-10" />
+        <p className="text-white text-2xl">InfoBot</p>
       </div>
-      <div className="flex flex-row items-center justify-evenly gap-3 ">
-        <p className="text-white text-3xl p-3 mb-2 border-l-2 border-white">
-          Features
-        </p>
-        <p className="text-white text-3xl p-3 mb-2 border-l-2 border-white">
-          Progress
-        </p>
-        <p className="text-white text-3xl mr-2 p-3 mb-2 border-l-2 border-white">
-          WishList
-        </p>
+      <div
+        className="absolute  right-0 md:right-5 top-5 hover:scale-110 transition-all ease-in-out duration-200 hover:cursor-pointer"
+        onClick={() => {
+          window.location.hostname = "os.infobot.tech";
+        }}
+      >
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-yellow-400 bg-transparent border-2 border-white p-2 rounded-lg">
+            Check Out the OS
+          </p>
+        </div>
       </div>
     </div>
   );
